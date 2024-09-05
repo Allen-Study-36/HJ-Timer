614696b36ec0c31f92c4346c2fb0841a5958b257
hyejeong <dev.imhj7@gmail.com>
2024-09-05 12:53:28 +0900

    feat: timer logic
    타이머 실행/취소

--- Timer/Timer/ViewController.swift
+++ Timer/Timer/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    var number = 0
+
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
-
-
+    
+    // MARK: UI
+    func configureUI() {
+        number = 0
+        mainLabel.text = "시간을 설정하세요"
+        slider.value = 0.5
+    }
+    
+    // MARK: @IBAction
+    // 슬라이더 값 변경
+    // -- UI 업데이트
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let seconds = Int(sender.value * 60)
+        mainLabel.text = "\(seconds) 초"
+        number = seconds
+    }
+    
+    // 타이머 시작
+    @IBAction func startTimer(_ sender: UIButton) {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+    }
+    
+    // 타이머 실행동작
+    @objc func timerAction() {
+        if number > 0 {
+            number -= 1
+            slider.value = Float(number) / Float(60)
+            mainLabel.text = "\(number) 초"
+        } else {
+            timer?.invalidate()
+            AudioServicesPlaySystemSound(SystemSoundID(1000))
+            configureUI()
+        }
+    }
+    
+    // 타이머 취소
+    @IBAction func cancelTimer(_ sender: UIButton) {
+        timer?.invalidate()
+        configureUI()
+    }
 }
+
 
